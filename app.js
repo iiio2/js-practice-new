7586feceb72425b00dd146c72eae7ea4809b1d04
iiio2 <gomexxxx71@gmail.com>
2021-03-24 22:07:03 +0600

    factory function.

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
-const firstName = 'Adam';
-const lastName = 'Smith';
+// const firstName = 'Adam';
+// const lastName = 'Smith';
 
-const fullName = `My name is ${firstName} ${lastName}`;
+// const fullName = `My name is ${firstName} ${lastName}`;
 
-console.log(fullName);
+// console.log(fullName);
+
+// Factory Functions
+
+// A factory function is a function that returns a new object. //
+
+function createCircle(radius) {
+  return {
+    radius,
+    draw() {
+      console.log('draw');
+    },
+  };
+}
+
+const circle1 = createCircle(5);
+console.log(circle1);
+const circle2 = createCircle(2);
+console.log(circle2);
